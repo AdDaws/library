c210ee1c23e405cbd07033779cf13661d4888293
ImageTranslator <b3003391@my.shu.ac.uk>
2015-03-03 13:21:27 +0000

    Add Library, Book, Student and Course classes

---
+++ library/grails-app/domain/com/library/Book.groovy
@@ -0,0 +1,22 @@
+package com.library
+
+class Book {
+
+String title
+String author
+String isbn
+Date dateBorrowed
+Date returnDate
+String student
+
+    static constraints = {
+
+title()
+author()
+isbn()
+dateBorrowed()
+returnDate()
+student()
+
+    }
+}
---
+++ library/grails-app/domain/com/library/Student.groovy
@@ -0,0 +1,18 @@
+package com.library
+
+class Student {
+
+String name
+String email
+String studentId
+String course
+
+    static constraints = {
+
+name()
+email()
+studentID()
+course()
+
+    }
+}
